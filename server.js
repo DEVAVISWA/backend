309bd8c020b846e7dc8ef7f1ce7b55f8b69ce0e8
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-15 23:20:15 +0530

    simple web server using node.js

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const http= require('http');
+// import the module http
+const http = require('http');
 
-const hostname= '127.0.0.1'
-const port= 3000
+// define the server hostname and port number
+const hostname = '127.0.0.1'; // localhost
+const port = 3000;
 
-const server= http.createServer((req,res) => {
-    res.statusCode=200
-    res.setHeader('Content-Type','text/plain')
-    res.end('Hello world')
-})
+// create a server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
 
-server.listen(port,hostname,()=>{
-    console.log(`server is running at http://${hostname}:${port}`)
-})
+// make the server to listen to the defined portnumber
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
