18892fad932701d6dafcaf0a278b00cb39878ade
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-15 23:47:30 +0530

    try on my own node.js simple server

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-// import the module http
-const http = require('http');
+const http= require('http')
 
-// define the server hostname and port number
-const hostname = '127.0.0.1'; // localhost
-const port = 3000;
+const host= '127.0.0.1'
+const port = 3000
 
-// create a server
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+const server = http.createServer((request,response)=> {
+    response.statusCode=200
+    response.setHeader=('Content-Type','text/plain')
+    response.end('Hello this is a ending mesage')
+})
 
-// make the server to listen to the defined portnumber
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
-});
+server.listen(port,host,()=> {
+    console.log(`the server is running at http://${host}:${port}`)
+})
