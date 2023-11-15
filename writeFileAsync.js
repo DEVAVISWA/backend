8f15a4095550b990d2ef396c88f0332afa1ec985
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-16 02:32:21 +0530

    WRITING the txt file with sync aasync in node.js

---
+++ writeFileAsync.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const content = 'welcome students'
+
+fs.writeFile('./test.txt', content, err => {
+    if (err) console.log(err)
+    console.log('content written successfully')
+})
