fd3c43454d8236a00219a42de4ea4708687af56b
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-16 23:59:25 +0530

    path.dirname,basename,extname

---
+++ path.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+const test= 'C:/Users/HP/Desktop/BackEnd/test.txt'
+
+// console.log(path.dirname(test))
+// console.log(path.basename(test))
+// console.log(path.extname(test))
+console.log(path.resolve('test.txt'))  
