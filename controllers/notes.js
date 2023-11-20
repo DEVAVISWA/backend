71a9ef80970ecf696db5b2056f9217d269c2a687
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-21 02:05:40 +0530

    cleaned and added DELETE a single note

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,36 +1,53 @@
-const notesRouter= require('express').Router()
-const Note= require('../models/note')
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
 
 
 //endpoint to GET all the notes
-notesRouter.get('/', (req,res)=> {   
-    Note.find({},{})
-        .then(notes=> {
+notesRouter.get('/', (req, res) => {
+    Note.find({}, {})
+        .then(notes => {
             res.status(200).json(notes)
         })
 })
 
 //end point to POST a  new data 
-notesRouter.post('/', (req,res)=> {
-    const note= new Note(req.body)
+notesRouter.post('/', (req, res) => {
+    const note = new Note(req.body)
     note.save()
-        .then(()=> {
-            res.status(201).json({message:'note created successfully'})
+        .then(() => {
+            res.status(201).json({ message: 'note created successfully' })
         })
 })
 
 //end point to GET a single note based on id
-notesRouter.get('/:id' , (req,res)=> {
+notesRouter.get('/:id', (req, res) => {
     //get the id from params
-    const id=req.params.id
+    const id = req.params.id
     //use mongoose method
     Note.findById(id)
-        .then(note => {            
-            res.status(200).json(note)            
+        .then(note => {
+            res.status(200).json(note)
         })
-        .catch (err => {
-            res.status(404).json({message: 'id dsnt exist'})
+        .catch(err => {
+            res.status(404).json({ message: 'id dsnt exist' })
         })
 })
 
-module.exports= notesRouter;
+//end point to DELETE a single resource based on id
+notesRouter.delete('/:id', (req, res) => {
+    const id = req.params.id
+    Note.findByIdAndDelete(id)
+        .then(deletedNote => {
+            if (deletedNote) {
+                res.status(204).json({ message: 'note deletes successfully' })
+            } else {
+                res.status(404).json({ message: 'id dsnt exits' })
+            }
+        })
+        .catch(err => {
+            res.status(404).json({ message: 'error deleting the note' })
+        })
+})
+
+
+module.exports = notesRouter;
