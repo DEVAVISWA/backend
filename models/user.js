5524a09a199a901af34f35105fbc943a5de30d4f
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-22 01:05:28 +0530

    MINI TASK added a new collection called users and done CRUD for it correctly with steps

--- models/user.js
+++ models/user.js
@@ -1,13 +1,25 @@
 const { default: mongoose } = require("mongoose");
 
-const userSchema= new mongoose.Schema({  //1
-    name: String,
-    email:String,
-    password:Number,
-    createdAt: String,
-    updatedAt: String
+const userSchema= new mongoose.Schema({  //2
+    name: {
+        type: String
+    },
+    email:{
+        type: String
+    },
+    password: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
-const User = mongoose.model('User', userSchema, 'users') //2
+const User = mongoose.model('User', userSchema, 'users') //1
 
 module.exports = User
